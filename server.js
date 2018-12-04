4c1b10af48370ae9138cf6a66ca954a0de79fa8c
tomddolezal <thomas.dolezal@mail.utoronto.ca>
2018-12-04 16:17:50 -0500

    a

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const sessionChecker = (req, res, next) => {
 };
 
 // route for root; redirect to login
-app.get("/", sessionChecker, (req, res) => {
+app.get("/",  (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
